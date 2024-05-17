7d3bd97d42191d236c1ea098bf8d1cad04217442
rafaseron <rafaelseron3011@gmail.com>
2024-05-17 16:25:08 -0400

    Adicionado ZStack para Profundidade. Usado GeometryReader{ } para pegar por 'lambda' o tamanho da tela do usuario

--- CheffDelivery/ui/Screens/LaunchScreen.swift
+++ CheffDelivery/ui/Screens/LaunchScreen.swift
@@ -11,24 +11,43 @@ struct LaunchScreen: View {
     @State private var isAnimating: Bool = false
     
     var body: some View {
-        VStack{
-            Text("Cheff Delivery")
-                .font(.system(size: 40))
-                .fontWeight(.heavy)
-                .foregroundStyle(.colorRed)
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Text("Peça as suas comidas no conforto da sua casa")
-                .font(.title2)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.black .opacity(0.7))
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Spacer()
-        }.onAppear{
-            withAnimation(.easeInOut(duration: 1.5)){
-                isAnimating = true
+        GeometryReader { geometry in
+            ZStack {
+                
+                Circle()
+                    .foregroundStyle(.colorRed)
+                    .frame(width: 200)
+                    .position(x: 50, y: 100)
+                    .blur(radius: 60)
+                    .opacity(0.5)
+                
+                Circle()
+                    .foregroundStyle(.colorRedDark)
+                    .frame(width: 200)
+                    .position(x: geometry.size.width-50, y: geometry.size.height-50)
+                    .blur(radius: 60)
+                    .opacity(0.5)
+                
+                VStack{
+                    Text("Cheff Delivery")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.colorRed)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Text("Peça as suas comidas no conforto da sua casa")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.black .opacity(0.7))
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Spacer()
+                }.onAppear{
+                    withAnimation(.easeInOut(duration: 1.5)){
+                        isAnimating = true
+                    }
+            }
             }
         }
     }
